bad6dc6c5f39201d8c0c0f11931d25dd3e117da9
Ilkay Beydah Saglam <beydahsaglam@hotmail.com>
2023-09-03 17:14:34 +0300

    Delete 15.Ders.sql

--- 15.Ders.sql
+++
@@ -1,7 +0,0 @@
---use Kitaplik
-
---select * from Kitap
-
---select KitapFiyat as 'Kitap Ucreti', KitapAd as 'Kitap Isim' from Kitap
-
-select KitapAd, KitapYazar, KitapFiyat, KitapFiyat * 5 as 'Yeni Fiyat' from Kitap
